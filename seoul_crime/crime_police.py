40ddbff83fa2ca376f69c5e8682c16ce064b2efe
hwanyhee <hwanyhee@naver.com>
2019-08-24 16:30:24 +0900

    190824 16:25

--- seoul_crime/crime_police.py
+++ seoul_crime/crime_police.py
@@ -14,3 +14,10 @@ class CrimeModel:
         self.dr.fname='crime_in_seoul.csv'
         crime=self.dr.csv_to_dframe()
         print(crime)
+        station_names =[]
+        for name in crime['관서명']:
+            station_names.append('서울'+str(name[:-1]+'경찰서'))
+        station_addrs =[]
+        station_lats = [] #위도
+        station_lngs = [] #경도
+        gmaps = self.dr.create_gmaps()
